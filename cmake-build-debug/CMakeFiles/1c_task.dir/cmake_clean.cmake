fbc9456b04bb98957f2e68420e3b094fe9da15fd
KemovaAnastasia <kemova.a.y@list.ru>
2021-04-12 22:31:55 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/1c_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1c_task"
+  "1c_task.pdb"
+  "CMakeFiles/1c_task.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1c_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
